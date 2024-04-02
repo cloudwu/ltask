df417b6b11e587fefd7906e4a77738fec795cae0
actboy168 <actboy168@gmail.com>
2024-04-02 23:13:47 +0800

    extract common bootstrap code

--- test/start.lua
+++ test/start.lua
@@ -1,63 +1,12 @@
 local boot = require "ltask.bootstrap"
 
-local SERVICE_ROOT <const> = 1
-local MESSSAGE_SYSTEM <const> = 0
-
-local config
-
 local function searchpath(name)
-	return assert(package.searchpath(name, config.service_path))
+	return assert(package.searchpath(name, "lualib/?.lua"))
 end
 
 local function readall(path)
 	local f <close> = assert(io.open(path))
 	return f:read "a"
-end
-
-local function init_config()
-	local servicepath = searchpath "service"
-	config.service_source = config.service_source or readall(servicepath)
-	config.service_chunkname = config.service_chunkname or ("@" .. servicepath)
-	config.initfunc = ([=[
-local name = ...
-package.path = [[${lua_path}]]
-package.cpath = [[${lua_cpath}]]
-local filename, err = package.searchpath(name, "${service_path}")
-if not filename then
-	return nil, err
-end
-return loadfile(filename)
-]=]):gsub("%$%{([^}]*)%}", {
-	lua_path = config.lua_path or package.path,
-	lua_cpath = config.lua_cpath or package.cpath,
-	service_path = config.service_path,
-})
-end
-
-local function new_service(label, id)
-	local sid = assert(boot.new_service(label, config.service_source, config.service_chunkname, id))
-	assert(sid == id)
-	return sid
-end
-
-local function bootstrap()
-	new_service("root", SERVICE_ROOT)
-	boot.init_root(SERVICE_ROOT)
-	-- send init message to root service
-	local init_msg, sz = boot.pack("init", {
-		initfunc = config.initfunc,
-		name = "root",
-		args = {config}
-	})
-	-- self bootstrap
-	boot.post_message {
-		from = SERVICE_ROOT,
-		to = SERVICE_ROOT,
-		session = 1,	-- 1 for root init
-		type = MESSSAGE_SYSTEM,
-		message = init_msg,
-		size = sz,
-	}
 end
 
 function print(...)
@@ -70,17 +19,36 @@ function print(...)
 	boot.pushlog(boot.pack("info", message))
 end
 
-local function start(cfg)
-	config = cfg
-	init_config()
-	boot.init(config)
-	boot.init_timer()
-	boot.init_socket()
-	bootstrap()	-- launch root
-	print "ltask Start"
-	local ctx = boot.run()
-	boot.wait(ctx)
-	boot.deinit()
+return function (config)
+	local servicepath = searchpath "service"
+	local root_config = {
+		bootstrap = config.bootstrap,
+		service_source = readall(servicepath),
+		service_chunkname = "@" .. servicepath,
+		initfunc = ([=[
+local name = ...
+package.path = [[${lua_path}]]
+package.cpath = [[${lua_cpath}]]
+local filename, err = package.searchpath(name, "${service_path}")
+if not filename then
+	return nil, err
 end
+return loadfile(filename)
+]=]):gsub("%$%{([^}]*)%}", {
+			lua_path = package.path,
+			lua_cpath = package.cpath,
+			service_path = config.service_path,
+		}),
+	}
 
-return start
+	boot.init_socket()
+	local bootstrap = dofile(searchpath "bootstrap")
+	local ctx = bootstrap.start {
+		core = config.core or {},
+		root = root_config,
+		root_initfunc = root_config.initfunc,
+		mainthread = config.mainthread,
+	}
+	print "ltask Start"
+	bootstrap.wait(ctx)
+end
