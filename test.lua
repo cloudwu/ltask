1b65dd4949a244262585638910fa301e8ea735d7
Hanchin Hsieh <me@yuchanns.xyz>
2025-10-01 17:28:46 +0800

    fix(ci/asan): bumped version of runner os

--- test.lua
+++ test.lua
@@ -2,6 +2,7 @@ local start = require "test.start"
 start {
     core = {
         debuglog = "=", -- stdout
+        worker = 3, -- avoid stuck when running ci on GHA macOS
     },
     service_path = "service/?.lua;test/?.lua",
     bootstrap = {
